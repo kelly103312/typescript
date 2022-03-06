3617fde48dcbaa3aa3a014fcc92f2e97812f2e9b
kelly103312 <cristaldemilcolores@gmail.com>
2022-03-06 00:43:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-xt3gpd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-xt3gpd)
---
+++ index.ts
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+let edad: number = 2;
+let nombre: string = 'maria';
+let esNina: Boolean = true;
+
+console.log(edad, nombre, esNina);
+//appDiv.innerHTML = edad.toString;
